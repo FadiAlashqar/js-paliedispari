9658db23dcaf13b899df202ee53dc79f22105037
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-23 17:21:06 +0100

    [UPD] ex 1 done

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 //definisco la funzione:
-function enterWord (word){
-
+function enterWord (Word, WordReverse){
+   if (Word === WordReverse) {
+        console.log('La parola è palindroma')
+   }
+   else{
+        console.log('La parola non è palindroma')
+   }
 };
 
 // inizilizzo la variabile dove andrà inserita la parola:
 let userWord = prompt('Inserisci una parola')
+let reversedWord = userWord.split("").reverse().join("");
 
 //chiamo la funzione:
-enterWord();
+enterWord(userWord, reversedWord);
+
