a6c8619a898d3e60b51ab337bd86441100539a9d
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-23 16:41:18 +0100

    [UPD] structure ex 1 done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+//definisco la funzione:
+function enterWord (word){
+
+};
+
+// inizilizzo la variabile dove andrà inserita la parola:
+let userWord = prompt('Inserisci una parola')
+
+//chiamo la funzione:
+enterWord();
